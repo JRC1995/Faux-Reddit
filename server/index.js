2aac68da8250b05d87c512c44c57aa3805315823
Jishnu Ray Chowdhury <jishnu.ray.c@gmail.com>
2019-05-02 17:58:07 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
+// IMPORT STUFFS
+
 const mysql = require('mysql');
 const express = require('express');
 var cors = require('cors')
 var Config = require('./config.json');
+
+// MIDDLEWARE
+
+const app = express();
+app.use(cors());
+
+// ESTABLISH MYSQL CONNECTION
+// https://github.com/mysqljs/mysql
 
 const db = mysql.createConnection({
   host     : Config.host,
@@ -9,8 +19,6 @@ const db = mysql.createConnection({
   password : Config.password,
   database : Config.database
 });
-
-// https://github.com/mysqljs/mysql
 
 db.connect(function(err) {
   if (err) {
@@ -21,12 +29,62 @@ db.connect(function(err) {
   console.log('connected as id ' + db.threadId);
 });
 
-const app = express();
-app.use(cors())
+// ESTABLISH MONGODB CONNECTION
+
+//const mongoDB = [write code]
+
+// RETRIEVE THREADS FOR FRONTPAGE
 
 app.get('/frontpage_threads',(req,res)=>{
 
-  let sql = 'SELECT * FROM thread t JOIN user_details u ON u.user_id = t.user_id WHERE removed=0 ORDER BY created_utc DESC LIMIT 100';
+  param = JSON.parse(req.query.param);
+
+  let sql_component = '';
+  var i = 0;
+  var values = []
+
+  for (const [key, value] of Object.entries(param)) {
+
+    values.push(value)
+    if (i==0)
+      {sql_component += 'AND (t.subforum_id = ?';}
+    else
+      {sql_component += ' OR t.subforum_id = ?';}
+    i+=1
+
+  }
+
+  if (i != 0){
+    sql_component += ") "
+  }
+
+  let sql = 'SELECT * \
+             FROM thread t JOIN user_details u ON u.user_id = t.user_id \
+             WHERE removed=0 '+sql_component+'\
+             ORDER BY CAST(t.created_utc/100 AS INT) DESC, t.score DESC \
+             LIMIT 100';
+
+  db.query({
+    sql: sql,
+    timeout: 40000, // 40s
+    values: values
+  }, function (error, results, fields) {
+    if (error) {
+      console.error('error connecting: ' + error.stack);
+      return;
+    }
+    res.send(results);
+    // error will be an Error if one occurred during the query
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+  });
+});
+
+// RETRIEVE CATEGORIES FOR SIDE BAR IN FRONTPAGE
+
+app.get('/categories',(req,res)=>{
+
+  let sql = 'SELECT * FROM category';
 
   db.query({
     sql: sql,
@@ -36,16 +94,32 @@ app.get('/frontpage_threads',(req,res)=>{
       console.error('error connecting: ' + error.stack);
       return;
     }
-    console.log(results)
     res.send(results);
-    console.log(fields);
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
   });
-
-
 });
+
+// RETRIEVE SUBCATEGORIES (SUBFORUMS) FOR SIDE BAR IN FRONTPAGE
+
+app.get('/subcategories',(req,res)=>{
+
+  category_id = req.query.category_id;
+
+  let sql = 'SELECT * FROM subforum WHERE category_id = ?';
+
+  db.query({
+    sql: sql,
+    timeout: 40000,
+    values: [category_id]
+  }, function (error, results, fields) {
+    if (error) {
+      console.error('error connecting: ' + error.stack);
+      return;
+    }
+    res.send(results);
+  });
+});
+
+// ESTABLISH SERVER PORT
 
 const PORT = process.env.PORT || 5000;
 
