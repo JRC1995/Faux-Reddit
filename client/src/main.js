60c1ee3cde58eb8fa37fe80ed1c979991a3b5547
Jishnu Ray Chowdhury <jishnu.ray.c@gmail.com>
2019-05-13 16:50:25 -0500

    Add files via upload

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
 import CKEditor from '@ckeditor/ckeditor5-vue';
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
 Vue.use( CKEditor );
 
 new Vue({
